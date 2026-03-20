42f47b87e6731f6886a675550f3a6987fc9d346a
florent <furien2001@gmail.com>
2026-03-20 13:36:30 +0100

    Chargement épisode 2

---
+++ python/moteur_ia.py
@@ -0,0 +1,28 @@
+import sys
+
+from predictor import ONNXPredictor
+
+
+def charger_modele(model_path: str) -> ONNXPredictor:
+    """
+    On charge le modèle IA
+
+    :param model_path: Le modèle ONNX a charger
+    :return: Le modèle chargé
+    """
+    print(f"info string Chargement du Modèle {model_path}", flush=True)
+    sys.stdout.flush()  # Indispensable pour l'envoi immédiat
+
+    try:
+        model = ONNXPredictor(model_path + ".onnx")
+
+    except Exception as e:
+        print(f"info string Erreur fatale : {str(e)}", flush=True)
+        return None
+
+    if model is None:
+        print(f"info string Le chargement du Modèle {model_path} a échoué", flush=True)
+    else:
+        print(f"info string Le chargement du Modèle {model_path} a réussi", flush=True)
+
+    return model
